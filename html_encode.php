bbbb0ffdc057ef37ecb5e9aa64744dc125d4d3c4
M-eporo <155133725+M-eporo@users.noreply.github.com>
2024-05-01 20:16:08 +0900

    Update html_escape.php

--- html_encode.php
+++ html_encode.php
@@ -1,5 +1,5 @@
 <?php 
-  function e(string $str, string $charset = 'UTF-8'): string {
+  function html_escape(string $str, string $charset = 'UTF-8'): string {
     return htmlspecialchars($str, ENT_QUOTES | ENT_HTML5, $charset, false);
   }
 ?>
